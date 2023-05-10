617ab68b3843212738ef8aa30aa251ba84ed5ceb
mehmetyasincirakpy <myasincirak@gmail.com>
2023-05-10 22:28:02 +0300

    .

--- lineer_regression.py
+++ lineer_regression.py
@@ -9,6 +9,7 @@ pandas.set_option("display.float_format", lambda x: "%.2f" % x)
 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 dataFrame = pandas.read_csv("datasets/advertising.csv")
 dataFrame.head()
@@ -57,3 +58,27 @@ mean_absolute_error(y, y_prediction)
 # R_KARE
 
 regression_model.score(X, y)
+
+# Multiple Linear Regression
+
+X = dataFrame.drop("sales", axis=1)
+y = dataFrame[["sales"]]
+
+# Modelleme
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+
+multiRegression_model = LinearRegression().fit(X_train, y_train)
+
+multiRegression_model.intercept_
+multiRegression_model.coef_
+
+#Train RMSE
+y_prediction=multiRegression_model.predict(X_train)
+numpy.sqrt(mean_squared_error(y_train,y_prediction))
+
+#Train RKARE
+multiRegression_model.score(X_train,y_train)
+
+y_prediction=multiRegression_model.predict(X_test)
+numpy.sqrt(mean_squared_error(y_test,y_prediction))
